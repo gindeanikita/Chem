9e6cba7a2e2d375c9a309889b31909d3e4195fcd
gindeanikita <gindeanikita@outlook.com>
2022-04-12 11:49:04 +0200

    Update 12.04.2022 11.49

--- R/ImportSDF.R
+++ R/ImportSDF.R
@@ -1,10 +1,22 @@
-ImportSDF <- function(files){
-  x <- 1
-  for(val in files) {
-    importedData <- new("SDFset")
-    importedData[[x]] <- ChemmineR::read.SDFset(val)
-    x <- x + 1
+ImportSDF <- function(origin, files){
+  if(origin == "list"){
+    x <- 1
+    for(val in files){
+      importedData <- new("SDFset")
+      importedData[[x]] <- ChemmineR::read.SDFset(val)
+      x <- x + 1
+    }
   }
-  return(importedData)
+
+  if(origin == "directory"){
+    x <- 1
+    SDFMolFiles <- list.files(pattern = c(".sdf", ".mol"), all.files = FALSE)
+    for(val in SDFMolFiles){
+      importedData <- new("SDFset")
+      importedData[[x]] <- ChemmineR::read.SDFset(val)
+      x <- x + 1
+    }
+  }
   print("Data imported")
+  return(importedData)
 }
