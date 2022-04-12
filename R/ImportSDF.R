85f8ac0ac1f1b900f7b4e799661877825d09764d
gindeanikita <gindeanikita@outlook.com>
2022-04-12 11:20:58 +0200

    Update 12.04.2022 11.20

--- R/ImportSDF.R
+++ R/ImportSDF.R
@@ -1,9 +1,10 @@
 ImportSDF <- function(files){
   x <- 1
   for(val in files) {
-    data[x,] <- ChemmineR::read.SDFset(val)
+    importedData <- new("SDFset")
+    importedData[[x]] <- ChemmineR::read.SDFset(val)
     x <- x + 1
   }
-  return(data)
+  return(importedData)
   print("Data imported")
 }
