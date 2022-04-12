6648a281d0e9b36a2ef5a37d123b0031e85892ed
gindeanikita <gindeanikita@outlook.com>
2022-04-12 16:23:47 +0200

    ImportSDF update

--- R/ImportSDF.R
+++ R/ImportSDF.R
@@ -1,11 +1,19 @@
-ImportSDF <- function(origin, files){
+ImportSDF <- function(origin, sdftype, files){
   importedData <- new("SDFset")
   x <- 1
 
   if(origin == "list"){
-    for(val in files){
-      importedData[[x]] <- ChemmineR::read.SDFset(val)
-      x <- x + 1
+    if(sdftype == "set"){
+      for(val in files){
+        importedData[[x]] <- ChemmineR::read.SDFset(val)
+        x <- x + 1
+      }
+    }
+    if(sdftype == "str"){
+      for(val in files){
+        importedData[[x]] <- ChemmineR::read.SDFstr(val)
+        x <- x + 1
+      }
     }
     str1 <- "List imported with "
     str2 <- toString(x - 1)
@@ -15,9 +23,17 @@ ImportSDF <- function(origin, files){
 
   if(origin == "directory"){
     SDFMolFiles <- list.files(pattern = c(".sdf", ".mol"), all.files = FALSE)
-    for(val in SDFMolFiles){
-      importedData[[x]] <- ChemmineR::read.SDFset(val)
-      x <- x + 1
+    if(sdftype == "set"){
+      for(val in SDFMolFiles){
+        importedData[[x]] <- ChemmineR::read.SDFset(val)
+        x <- x + 1
+      }
+    }
+    if(sdftype == "str"){
+      for(val in SDFMolFiles){
+        importedData[[x]] <- ChemmineR::read.SDFstr(val)
+        x <- x + 1
+      }
     }
     str1 <- "Directory imported with "
     str2 <- toString(x - 1)
